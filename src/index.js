5c08cbaf2a7da21942f097db4858d568f8c07be8
Matt Harris <m@ttharr.is>
2020-10-28 22:55:58 -0400

    bugfixes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { unregister } from './registerServiceWorker';
-import IronHacksApp from './ironhackApp.js';
+import App from './ironhackApp.js';
 import 'bootstrap/dist/css/bootstrap-reboot.min.css';
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import './main.css';
